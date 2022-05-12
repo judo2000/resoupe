aa981a0c2d9f72d5197ed255b741fa9f31f0d310
Andrew McInally <realfunnyestate@yahoo.com>
2022-05-12 14:29:05 -0600

    created recipe card

---
+++ client/src/components/RecipeCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function RecipeCard({recipe}) {
+  return (
+    
+      <div >
+        <Link to={`/singlePage/${recipe.recipe.uri.split('#recipe_')[1]}`}>
+          <div className="mb-2 rounded-md card bg-yellow-800 hover:bg-yellow-900">
+            <div className="card-body">{recipe.recipe.label}</div>
+            <p>
+              <img src={recipe.recipe.images.THUMBNAIL.url} alt="recipe" />
+            </p>
+          </div>
+        </Link>
+      </div>
+    
+  )
+}
+
+export default RecipeCard
