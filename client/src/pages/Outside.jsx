745dab6b34ab6f6ec066afc29e32bbceba90f084
Stephen Moore <judo2000@comcast.net>
2022-05-12 07:51:10 -0600

    Create Outside page with edmam food api

---
+++ client/src/pages/Outside.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const url =
+  "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=eeb26e4a&app_key=e5fe80eaf283e5c29c518c34b7a65c94%09";
+
+const singleRecipe_url =
+  "https://api.edamam.com/api/recipes/v2/b79327d05b8e5b838ad6cfd9576b30b6?type=public&app_id=eeb26e4a&app_key=e5fe80eaf283e5c29c518c34b7a65c94%09&field=uri&field=label&field=image&field=images&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=glycemicIndex&field=totalCO2Emissions&field=co2EmissionsClass&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily&field=digest";
+
+const url_elements = singleRecipe_url
+  .split("v2/")
+  .join(", ")
+  .split("?")
+  .join(", ")
+  .split(", ");
+
+const recipe_id = console.log(url_elements[1]);
+function Outside() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setRecipes(data.hits);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={recipe.recipe.uri.split("#recipe_")[1]}>
+            <Link to={`/singlePage/${recipe.recipe.uri.split("#recipe_")[1]}`}>
+              {recipe.recipe.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Outside;
