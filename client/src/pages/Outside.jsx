4cc9a5c6936b4281d53aa90c5948fae8bca69b17
Stephen Moore <judo2000@comcast.net>
2022-05-12 11:47:45 -0600

    Remove unnecessary code from outside page

--- client/src/pages/Outside.jsx
+++ client/src/pages/Outside.jsx
@@ -4,17 +4,9 @@ import { Link } from "react-router-dom";
 const url =
   "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=eeb26e4a&app_key=e5fe80eaf283e5c29c518c34b7a65c94%09";
 
-const singleRecipe_url =
-  "https://api.edamam.com/api/recipes/v2/b79327d05b8e5b838ad6cfd9576b30b6?type=public&app_id=eeb26e4a&app_key=e5fe80eaf283e5c29c518c34b7a65c94%09&field=uri&field=label&field=image&field=images&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=glycemicIndex&field=totalCO2Emissions&field=co2EmissionsClass&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily&field=digest";
+// const singleRecipe_url =
+//   "https://api.edamam.com/api/recipes/v2/b79327d05b8e5b838ad6cfd9576b30b6?type=public&app_id=eeb26e4a&app_key=e5fe80eaf283e5c29c518c34b7a65c94%09&field=uri&field=label&field=image&field=images&field=source&field=url&field=shareAs&field=yield&field=dietLabels&field=healthLabels&field=cautions&field=ingredientLines&field=ingredients&field=calories&field=glycemicIndex&field=totalCO2Emissions&field=co2EmissionsClass&field=totalWeight&field=totalTime&field=cuisineType&field=mealType&field=dishType&field=totalNutrients&field=totalDaily&field=digest";
 
-const url_elements = singleRecipe_url
-  .split("v2/")
-  .join(", ")
-  .split("?")
-  .join(", ")
-  .split(", ");
-
-const recipe_id = console.log(url_elements[1]);
 function Outside() {
   const [recipes, setRecipes] = useState([]);
 
@@ -22,7 +14,6 @@ function Outside() {
     const fetchData = async () => {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
       setRecipes(data.hits);
     };
 
