1a65a14e6ffc1740c7d5fe17766c2718275637f2
Andrew McInally <realfunnyestate@yahoo.com>
2022-05-14 11:07:40 -0600

    added search

---
+++ client/src/components/SearchForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function SearchForm() {
+  const [text, setText] = useState('')
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text === '') {
+      alert('Please enter a search')
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="chicken"
+        value={text}
+        onChange={handleChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  )
+}
+
+export default SearchForm
